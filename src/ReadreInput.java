7b063fedb5c6194999bce626e5a9eb70b9176a89
Lab <Lab@10.9.26.151>
2016-11-03 11:15:46 +0700

    Lab9 project

---
+++ src/ReadreInput.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class ReadreInput {
+
+	public static void main(String[] args) throws IOException{
+		Reader.init(new FileInputStream(new File("data/input.txt")));
+		Reader.setDelimeter("\t,");
+		int year;
+		double amount,rate = 5.0;
+		double mPay,tPay,mRate;
+		amount = Reader.nextDouble();
+		year = Reader.nextInt();
+		mRate = rate / 1200;
+		mPay = (mRate * amount)/(1-(1/Math.pow(1 + mRate, year * 12)));
+		tPay = mPay * (year * 12);
+		
+		System.out.printf("Loan Amount : %10.2f\n",amount);
+		System.out.printf("Naber of Years : %10d\n",year);
+		System.out.println("Interest Rate\t\tMonthly Payment\t\tTotal Payment");
+		System.out.printf("%15.2f\t\t%15.2f\t\t%20.2f\n",rate,mPay,tPay);
+	}//main
+
+}//class
